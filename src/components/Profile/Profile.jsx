b9dfa41dc08fc609a4dc27ccb430b709d4ec2909
brizhak <ada.adelina17@gmail.com>
2023-07-12 13:31:08 +0300

    propTypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const Profile = ({username, tag, location, avatar, stats}) => {
     return (
     <div className={"profile"}>
@@ -28,6 +30,14 @@ const Profile = ({username, tag, location, avatar, stats}) => {
   </ul>
 </div>
     )
+}
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired, 
+  location: PropTypes.string.isRequired, 
+  avatar: PropTypes.string, 
+  stats: PropTypes.object,
 }
 
 export default Profile;
